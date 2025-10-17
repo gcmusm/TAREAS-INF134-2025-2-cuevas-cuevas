6999bc11da1fc1b904bf9498f73887dc9184affb
agusbop <gcuevasm@usm.cl>
2025-10-17 19:10:05 -0300

    fix

--- abb.cpp
+++ abb.cpp
@@ -1,9 +1,17 @@
 #include "abb.h"
 
-void construir_arboles_por_palo(const carta base[52], nodo_abb* arboles[4]){
+
+void extraer_por_palo(const carta base[52], char palo, carta salida[13], int& n){
+
+}
+nodo_abb* construir_balanceado(const carta v[], int inicio, int fin){
 
 }
 
+void construir_arboles_por_palo(const carta base[52], nodo_abb* arboles[4]){
+    
+
+}
 void inorden(nodo_abb* raiz, void (*visitar)(const carta&)){
 
 }
@@ -12,10 +20,4 @@ nodo_abb* buscar_por_categoria(nodo_abb* raiz, int categoria){
 }
 void destruir_arbol(nodo_abb*& raiz){
 
-}
-void extraer_por_palo(const carta base[52], char palo, carta salida[13], int& n){
-
-}
-nodo_abb* construir_balanceado(const carta v[], int inicio, int fin){
-    
 }
