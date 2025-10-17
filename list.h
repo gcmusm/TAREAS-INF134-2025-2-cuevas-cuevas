9b6301647339480aeede708f12270f9adf2dc1c5
agusbop <gcuevasm@usm.cl>
2025-10-16 22:14:02 -0300

    list.h

--- list.h
+++ list.h
@@ -12,7 +12,10 @@ struct lista{
     lista() : head(nullptr) {}
 }
 
-void append(Lista& L, const carta& c);
-
-
+void append(lista& L, const carta& c);
+bool eraseen(lista& L, int pos);          
+int  length(const lista& L);              
+bool get(const lista& L, int pos, carta&);  
+void clear(lista& L);                       
+bool popFront(lista& L, carta& out);  
 #endif
