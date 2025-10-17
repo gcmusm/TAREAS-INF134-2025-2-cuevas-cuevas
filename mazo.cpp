03d6b0b6cbbbd11beb92e5a33ea77f1d4753cf32
condevalentain <141572119+condevalentain@users.noreply.github.com>
2025-10-17 19:05:18 +0000

    10

--- mazo.cpp
+++ mazo.cpp
@@ -5,8 +5,8 @@ void cargarMazo(lista& mazo, const carta base[52], const int orden[52]){
     clear(mazo);
     for(int pos=0; pos<NUM; pos++){
         int ibase= orden[pos];
-        if(ibase>= 0 %% ibase<NUM){
-            append(mazo, base[ibase])
+        if(ibase>= 0 && ibase<NUM){
+            append(mazo, base[ibase]);
         }
     }
 }  
@@ -16,7 +16,7 @@ void repartir(lista& mazo, lista& mano, int n){
     bool haycartas = true;
     carta cartasacada;
     while(haycartas==true && entregadas<n){
-        haycartas= popfront(mazo, cartasacada):
+        haycartas= popfront(mazo, cartasacada);
         if(haycartas==true){
             append(mano, cartasacada);
             entregadas++;
@@ -26,9 +26,21 @@ void repartir(lista& mazo, lista& mano, int n){
 }
 
 void reponerHasta(lista& mazo, lista& mano, int objetivo){
-
+    if (objetivo<=0) 
+        return;
+    int actuales=length(mano); 
+    while (actuales<objetivo) {
+        carta c;
+        if (!popfront(mazo,c)) 
+            break;  
+        append(mano,c);               
+        actuales++;
+    }
 }
 
 void moverTodo(lista& origen, lista& destino){
-    
+    carta c;
+    while (popfront(origen,c)) {
+        append(destino,c); 
+    }
 }
