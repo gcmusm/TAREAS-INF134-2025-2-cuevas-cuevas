e6808b2db92006a5ad06f32fd6fd25fe3f954b70
agusbop <gcuevasm@usm.cl>
2025-10-17 19:53:35 -0300

    balance

--- abb.cpp
+++ abb.cpp
@@ -2,13 +2,33 @@
 
 
 void extraer_por_palo(const carta base[52], char palo, carta salida[13], int& n){
-
+    n=0;
+    for(int i =0; i<52; i++){
+        if(base[i].palo == palo){
+            if(n<13){
+                salida[n] = base[i];
+                n++;
+            }
+        }
+    }
 }
 nodo_abb* construir_balanceado(const carta v[], int inicio, int fin){
-
+    if(inicio>fin){
+        return nullptr;
+    }
+    int medio = (inicio+fin)/2;
+    nodo_abb* raiz = new nodo_abb;
+    raiz->info = v[medio];  
+    raiz->izq = nullptr;
+    raiz->der = nullptr;
+    raiz->izq = construir_balanceado(v, inicio, medio-1);
+    raiz->der = construir_balanceado(v, medio+1, fin);
+    return raiz;    
 }
 
+
 void construir_arboles_por_palo(const carta base[52], nodo_abb* arboles[4]){
+    const char palos[4] = 
     
 
 }
