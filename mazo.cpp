9dabc90f897f4abb4d4d8cf0c86dfa8f82eca58e
agusbop <gcuevasm@usm.cl>
2025-10-17 00:07:12 -0300

    mazo

---
+++ mazo.cpp
@@ -0,0 +1,17 @@
+#include "mazo.h"
+
+void cargarMazo(lista& mazo, const carta base[52], const int orden[52]){
+
+}  
+
+void repartir(lista& mazo, lista& mano, int n){
+
+}
+
+void reponerHasta(lista& mazo, lista& mano, int objetivo){
+
+}
+
+void moverTodo(lista& origen, lista& destino){
+    
+}
---
+++ mazo.h
@@ -0,0 +1,16 @@
+#ifndef MAZO_H
+#define MAZO_H
+#include "carta.h"
+#include "list.h"
+
+void cargarMazo(lista& mazo, const carta base[52], const int orden[52]);  
+
+void repartir(lista& mazo, lista& mano, int n);
+
+void reponerHasta(lista& mazo, lista& mano, int objetivo);
+
+void moverTodo(lista& origen, lista& destino);
+
+
+
+#endif
