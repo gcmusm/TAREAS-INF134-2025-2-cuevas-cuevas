5770673a6fa2bb77531502eb5cde90b7613e710c
agusbop <gcuevasm@usm.cl>
2025-10-17 22:52:53 -0300

    inter

--- interfaz.cpp
+++ interfaz.cpp
@@ -16,14 +16,31 @@ void imprimir_lista(const lista& L){
     cout<< "]";
 }
 void imprimir_primeras(const lista& L, int n){
-
+    nodo* actual = L.head;
+    int i=0;
+    cout << "[";
+    while(actual !=nullptr && i<n){
+        imprimir_carta(actual->info);
+        actual = actual->sig;
+        i++;
+        if(actual !=nullptr && i<n){
+            cout <<"  ";
+        }   
+    }
+    cout << "]";
 }
 void mostrar_carta_inorden(const carta& c){
     imprimir_carta(c);
     cout << "  ";
 }
 void imprimir_inorden(nodo_abb* raiz){
-
+    if(raiz == nullptr){
+        cout << "(arbol vacio)";
+        return;
+    }
+    cout << "[";
+    inorden(raiz, mostrar_carta_inorden);
+    cout << "]";
 }
 void imprimir_estado(int fichas, int pozo, int quedan_mazo){
     
