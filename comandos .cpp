de97c248cb47dd005879e108e30094794f630e6a
agusbop <gcuevasm@usm.cl>
2025-10-18 00:32:50 -0300

    comandos

---
+++ comandos .cpp
@@ -0,0 +1,17 @@
+#include "comandos.h"
+
+bool indices_validos(const lista& mano, const int posiciones[], int cantidad){
+
+}
+bool copiar_cartas_por_posiciones(const lista& mano, const int posiciones[], int cantidad, carta cartas_salida[], int& cantidad_salida){
+
+}
+void ordenar_posiciones_desc(int posiciones[], int cantidad){
+
+}
+void descartar_y_reponer(lista& mano, lista& mazo, int posiciones[], int cantidad, int cantidad_objetivo){
+
+}
+bool preparar_jugada(const lista& mano, const int posiciones[], int cantidad, carta jugada[], int& cantidad_jugada){
+    
+}
